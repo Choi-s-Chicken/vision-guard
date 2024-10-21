0a6e5af53ea93fb66a04dc9c35118f7975d39ef1
suzukaotto <me@szk.kr>
2024-10-21 12:29:54 +0900

    update: code stabilty

--- src/face_reco.py
+++ src/face_reco.py
@@ -1,16 +1,11 @@
-import time
-import os
-import hashlib
 import cv2
 import numpy as np
 from openvino.runtime import Core
 
-
-
 class FaceRecognition:
-    def __init__(self, detection_model_path: str, reid_model_path: str, device: str = "CPU"):
+    def __init__(self, dtct_model_path: str, reid_model_path: str, device: str = "CPU"):
         self.core = Core()
-        self.detection_model = self.load_model(detection_model_path, device)
+        self.detection_model = self.load_model(dtct_model_path, device)
         self.reid_model = self.load_model(reid_model_path, device)
         self.detection_input_shape = self.detection_model.inputs[0].shape
         self.reid_input_shape = self.reid_model.inputs[0].shape
@@ -51,29 +46,24 @@ class FaceRecognition:
         return None
 
     def calculate_similarity(self, base_face_embedding, input_face_embedding):
-        # Thank you gpt...!!!
         base_norm = np.linalg.norm(base_face_embedding)
         input_norm = np.linalg.norm(input_face_embedding)
         similarity = np.dot(base_face_embedding, input_face_embedding) / (base_norm * input_norm)
         return similarity
 
     def compare_faces(self, base_face_path, input_face_path):
-        # 전처리
         base_image = self.preprocess_image(base_face_path)
         input_image = self.preprocess_image(input_face_path)
         
-        # 얼굴 추출
         base_face = self.extract_face(base_image)
         input_face = self.extract_face(input_image)
 
         if base_face is None or input_face is None:
             return "Could not detect one or both faces."
 
-        # 얼굴 임베딩 추출
         base_face_embedding = self.reid_model.infer_new_request({0: base_face})[self.reid_model.outputs[0]]
         input_face_embedding = self.reid_model.infer_new_request({0: input_face})[self.reid_model.outputs[0]]
 
-        # 유사도 계산
         similarity = self.calculate_similarity(base_face_embedding.flatten(), input_face_embedding.flatten())
         return similarity
 
