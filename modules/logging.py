d0283f27ee852039f52c61f6beb6888d6dc89ad9
suzukaotto <me@szk.kr>
2024-10-30 17:39:34 +0900

    update: code update

--- modules/logging.py
+++ modules/logging.py
@@ -3,7 +3,7 @@ import config
 
 _logging_format = "%(asctime)s [%(levelname)s] %(message)s"
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format=_logging_format,
                     encoding="utf-8",
                     filename=config.LOG_PATH)
